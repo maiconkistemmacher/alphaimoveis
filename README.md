32949da578f92d148850641ae3a250d5ba97a37d
maiconkistemmacher <maicon.kistemmacher@gmail.com>
2021-03-12 08:54:08 -0300

    update imovel

---
+++ README.md
@@ -0,0 +1,6 @@
+# Desafio - Vaga de Programador PHP na Alphacode
+
+## Projeto se trata de um site para imobiliária, e um painel administrativo para gerenciar os imóveis.
+
+
+
--- includes/imovel.php
+++ includes/imovel.php
@@ -2,8 +2,8 @@
       <div class="container">
         <div class="row align-items-center justify-content-center">
           <div class="col-md-6 mt-lg-5 text-center">
-            <h1>480 12th, Unit 14, San Francisco, CA</h1>
-            <p class="mb-5"><strong class="text-white">$3,4000,000</strong> &mdash; 6 Beds, 4 Baths, 4250 sqft.</p>
+            <h1>Residencial Cinco - Alphaville</h1>
+            <p class="mb-5"><strong class="text-white">R$3.4000,000</strong> &mdash; 6 Quartos, 4 Banheiros, 420 m2</p>
             
           </div>
         </div>
@@ -26,14 +26,14 @@
           </div>
           <div class="col-lg-5 pl-lg-5 ml-auto">
             <div class="mb-5">
-              <h3 class="text-black mb-4">Property Description</h3>
+              <h3 class="text-black mb-4">Descrição do Imóvel</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa tempore repudiandae optio aliquam perspiciatis est quae enim quaerat eos hic dolorem accusamus molestias repellat consequatur velit, officiis nihil magnam placeat!</p>
-              <p>Ex, esse? Obcaecati nam in cum eius quaerat repellendus adipisci ducimus dolorum sed quos. Amet recusandae cumque reprehenderit nam quia voluptatibus, repellat, aspernatur ut fuga perferendis consectetur excepturi neque in!</p>
+              <!-- <p>Ex, esse? Obcaecati nam in cum eius quaerat repellendus adipisci ducimus dolorum sed quos. Amet recusandae cumque reprehenderit nam quia voluptatibus, repellat, aspernatur ut fuga perferendis consectetur excepturi neque in!</p> -->
               <p class="mb-4">Neque facilis iure earum, placeat odit ipsum, amet, optio accusantium voluptatem quasi obcaecati fugit? Explicabo eius dolorem provident quis non voluptas, dignissimos tempora eligendi, in, nam velit, quasi tenetur. Animi!</p>
-              <p><a href="#" class="btn btn-primary">Buy Property</a></p>
+              <p><a href="index.php#contato" class="btn btn-primary">Entre em contato</a></p>
             </div>
 
-            <div class="mb-5">
+            <!-- <div class="mb-5">
               <h3 class="text-black mb-4">Contact Agent</h3>
               <div class="mt-5">
                 <img src="images/person_8.jpg" alt="Image" class="w-25 mb-3 rounded-circle">
@@ -42,7 +42,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, iure atque sit ratione vitae neque! Laborum voluptate eius, laboriosam explicabo!</p>
                 <p><a href="#" class="btn btn-primary btn-sm">Contact Agent</a></p>
               </div>
-            </div>
+            </div> -->
 
           </div>
         </div>
